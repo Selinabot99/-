e5fd4cac7183cf483392196257ab251a6a5e3924
Selinabot99 <113962831+Selinabot99@users.noreply.github.com>
2022-09-20 11:13:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94703427368'] //ur owner number
+global.ownername = "CHAMINDU IMANTHA 🪄🖇️" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94703427368']
+global.ownertag = '94703427368' //ur tag number
+global.botnumber = '94703427368' //Add your Bot Number For Log
+global.botname = '𝚂𝙴𝙻𝙸𝙽𝙰_ɢɪʀʟ BETA EDITION' //ur bot name
+global.ig = "https://wa.me/qr/BKPOQGSF4EGEE1" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.facebook.com/chamindu.imantha.3" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.packname = "𝚂𝙴𝙻𝙸𝙽𝙰_ɢɪʀʟ BoT" //ur sticker watermark packname
+global.author = "𝚂𝙴𝙻𝙸𝙽𝙰_ɢɪʀʟ" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
